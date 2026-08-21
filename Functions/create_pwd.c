15bc6b0382bf23294684db070b1797d3fda16770
CreatedBySarika <sahsarika2005@gmail.com>
2026-08-21 14:35:36 +0100

    create passwords

---
+++ Functions/create_pwd.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void createPassword(int length){   
+    char randomchar[length];
+    int ind=0;
+    int x=0;
+    //generate random ASCII printable char
+    for (int k=0;k<length;k++)
+    {
+        randomchar[x] = (char)(33+rand()%94);
+        x++;    
+    }
+
+    //print the generated random character in the array
+    for (int j=0;j<sizeof(randomchar);j++)
+    {
+        printf("%c",randomchar[j]);
+    }
+
+};
+int main()
+{
+    srand(time(NULL));
+    createPassword(5);
+}
