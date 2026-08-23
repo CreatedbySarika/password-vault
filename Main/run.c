0b43a278a3b202639ba55dd7d15df570e5f4fdef
CreatedBySarika <sahsarika2005@gmail.com>
2026-08-23 22:40:21 +0100

    switch case added

--- Main/run.c
+++ Main/run.c
@@ -4,22 +4,38 @@
 
 int main(){
     int length;
+    int option;
    // char[20] username;
     //char[20] password
 
     printf("Hellooo !! welcome to the password vault\n");
     read_File("../Files/Menu.txt");
+    scanf("%d",&option);
 
-    printf("How long do you want the you password is: ");
-    scanf("%d",&length);
-    while(length<8)
+    switch(option)
     {
-        printf("heyyy thats to small to have apassword make it atleast more than 8: ");
-        scanf("%d",&length);
+        case 1:
+            printf("How long do you want the you password is: ");
+            scanf("%d",&length);
+            while(length<8)
+            {
+                printf("heyyy thats to small to have apassword make it atleast more than 8: ");
+                scanf("%d",&length);
+            }
+
+            srand(time(NULL));
+            createPassword(length);
+            break;
+
+        case 2:
+            printf("this is case two");
+        case 3:
+            printf("this is case 3");
+            
+
     }
-    
-    srand(time(NULL));
-    createPassword(length);
+
+
     char username[12];
     printf("\nEnter Username: ");
     scanf("%s",username);
