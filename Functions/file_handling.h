f03238e614c639f00caa3d6ab70599836d690bce
CreatedBySarika <sahsarika2005@gmail.com>
2026-08-23 22:27:38 +0100

    reading file function

--- Functions/file_handling.h
+++ Functions/file_handling.h
@@ -8,3 +8,22 @@ void create_user(char username[12])
     fprintf(fp,"%s",username);
     fclose(fp);
 }
+
+void read_File(const char *file){
+    FILE *pf = fopen(file,"r");
+    char buffer[255];
+
+    if(pf == NULL)
+    {
+        printf("Unable to locate the file");
+    }
+
+    while((fgets(buffer,255,pf))!=NULL)
+    {
+        printf("%s",buffer);
+
+    }
+    fclose(pf);
+    
+
+}
